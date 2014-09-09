465ee473f9045e76ba5bba169a3107e27135f5a4
Chris Sinchok <csinchok@theonion.com>
2014-09-09 16:20:09 -0500

    Whitespace

--- comcastify.js
+++ comcastify.js
@@ -1,91 +1,73 @@
 var comcastifyjs = (function () {
+  // setup slowload modifier callback, structure avoids some annoying timer/closure problems
+  var slowloadModiferCallback = function (slowloadDiv, args) {
+    return function () {
+      (function (slowloadDiv, args) {
 
-    // setup slowload modifier callback, structure avoids some annoying timer/closure problems
-    var slowloadModiferCallback = function (slowloadDiv, args) {
+        // calculate new height
+        var newTopClip = slowloadDiv.slothifyData.imageTopClip + args.loadIncrement;
 
-         return function () {
+        // update slowload div
+        slowloadDiv.style.clip = 'rect(' + newTopClip + 'px auto auto auto)';
 
-            (function (slowloadDiv, args) {
+        // check stopping conditions
+        if (newTopClip < slowloadDiv.slothifyData.maxImageHeight) {
+          // create new update timeout
+          slowloadDiv.slothifyData.imageTopClip = newTopClip;
+          setTimeout(slowloadModiferCallback(slowloadDiv, args), args.loadSpeed);
+        }
+      })(slowloadDiv, args);
+    };
+  };
 
-                // calculate new height
-                var newTopClip = slowloadDiv.slothifyData.imageTopClip + args.loadIncrement;
+  var slowImages = function (args) {
+    return function () {
+      var params = {
+        boxColor: args.boxColor || '#000000',       // color of box overlay
+        loadMaxPercent: args.loadMaxPercent || 0.0, // max percentage to load images
+        loadSpeed: args.loadSpeed || 500,           // how often in ms to pass
+        loadIncrement: args.loadIncrement || 1      // pixels to load per pass
+      };
 
-                // update slowload div
-                slowloadDiv.style.clip = 'rect(' + newTopClip + 'px auto auto auto)';
+      // grab all the images
+      var imgs = document.getElementsByTagName('img');
 
-                // check stopping conditions
-                if (newTopClip < slowloadDiv.slothifyData.maxImageHeight) {
+      // make 'em load slow
+      for(var i = 0; i < imgs.length; i++) {
+        // get some things we need
+        var img = imgs[i],
+            parent = img.parentNode,
+            slowload = document.createElement('DIV');
 
-                    // create new update timeout
-                    slowloadDiv.slothifyData.imageTopClip = newTopClip;
-                    setTimeout(slowloadModiferCallback(slowloadDiv, args), args.loadSpeed);
+        // set up initial state of box
+        slowload.className = 'sloth-box';
+        slowload.style.backgroundColor = params.boxColor;
+        slowload.style.width = img.offsetWidth + 'px';
+        slowload.style.height = img.offsetHeight + 'px';
+        slowload.style.position = 'absolute';
+        slowload.style.top = img.offsetTop + 'px';
+        slowload.style.left = img.offsetLeft + 'px';
+        slowload.style.clip = 'rect(0 auto auto auto)';
 
-                }
-
-            })(slowloadDiv, args);
-
+        // remember what the max height should be for later calculation
+        slowload.slothifyData = {
+            imageTopClip: 0,
+            maxImageHeight: img.height * params.loadMaxPercent
         };
 
-    };
+        // put box over image
+        parent.appendChild(slowload);
 
-    var slowImages = function (args) {
-
-        return function () {
-
-            var params = {
-
-                boxColor: args.boxColor || '#000000',       // color of box overlay
-                loadMaxPercent: args.loadMaxPercent || 0.0, // max percentage to load images
-                loadSpeed: args.loadSpeed || 500,           // how often in ms to pass
-                loadIncrement: args.loadIncrement || 1      // pixels to load per pass
-
-            };
-
-            // loop through images and make them slow
-            var imgs = document.getElementsByTagName('img');
-            for(var i = 0; i < imgs.length; i++) {
-
-                // get some things we need
-                var img = imgs[i],
-                    parent = img.parentNode,
-                    slowload = document.createElement('DIV');
-
-                // set up initial state of box
-                slowload.className = 'sloth-box';
-                slowload.style.backgroundColor = params.boxColor;
-                slowload.style.width = img.offsetWidth + 'px';
-                slowload.style.height = img.offsetHeight + 'px';
-                slowload.style.position = 'absolute';
-                slowload.style.top = img.offsetTop + 'px';
-                slowload.style.left = img.offsetLeft + 'px';
-                slowload.style.clip = 'rect(0 auto auto auto)';
-
-                // remember what the max height should be for later calculation
-                slowload.slothifyData = {
-                    imageTopClip: 0,
-                    maxImageHeight: img.height * params.loadMaxPercent
-                };
-
-                // put box over image
-                parent.appendChild(slowload);
-
-                if (params.loadMaxPercent > 0.0) {
-
-                    // slowload using timeout since this is nicer to the browser :)
-                    setTimeout(slowloadModiferCallback(slowload, params), params.loadSpeed);
-
-                }
-
-            }
-
+        if (params.loadMaxPercent > 0.0) {
+            // slowload using timeout since this is nicer to the browser :)
+            setTimeout(slowloadModiferCallback(slowload, params), params.loadSpeed);
         }
+      }
+    }
+  };
 
-    };
-
-    return {
-
-        fixMyImagesLoadingSoFast: slowImages
-
-    };
+  return {
+    fixMyImagesLoadingSoFast: slowImages
+  };
 
 })();
